5bdfab0e0aa73e5585909e8e55c429eae527fb46
João Pablo Duarte <pablosuporte.pb@gmail.com>
2025-07-16 17:14:03 -0400

    Exercicio1/JoaoPablo
    Código do exercício 1

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+//Programa para soma de valores.
+
+let valor1 = 199.3;
+let valor2 = 120;
+
+//Calculo
+let soma = valor1 + valor2;
+
+//Resultado
+console.log("O valor da soma entre os dois valores é:",(soma));
